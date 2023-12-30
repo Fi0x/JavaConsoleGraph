d295f06b74e5c521145f1cf60b00418217ab1532
Fi0x <felpech@aol.com>
2023-12-30 14:21:02 +0100

    Updated dependencies, version and java-doc comments

--- src/main/java/io/fi0x/javaconsolegraph/Transformer.java
+++ src/main/java/io/fi0x/javaconsolegraph/Transformer.java
@@ -2,6 +2,10 @@ package io.fi0x.javaconsolegraph;
 
 import java.util.ArrayList;
 
+/**
+ * This class contains methods, to transform arrays and lists into a single String,
+ * that is formatted to display a nice looking graph or table.
+ */
 public class Transformer
 {
     private final ConsoleTable tableInstance;
@@ -16,6 +20,7 @@ public class Transformer
     }
     /**
      * Get the {@link Transformer}-singleton and create it if it does not exist yet.
+     *
      * @return The instance of the {@link Transformer}-singleton.
      */
     public static Transformer getInstance()
@@ -28,8 +33,9 @@ public class Transformer
 
     /**
      * Get a correctly formatted {@link String} that will print out a table.
+     *
      * @param columnHeaders The headers of each column that the table should have.
-     * @param columns The individual entries of each column.
+     * @param columns       The individual entries of each column.
      * @return A correctly formatted {@link String} that will create a table with the provided headers and entries.
      */
     public String getTable(String[] columnHeaders, String[][] columns)
@@ -48,6 +54,7 @@ public class Transformer
     }
     /**
      * Get a correctly formatted {@link String} that will print out a table.
+     *
      * @param columns The individual columns of the table, including their headers.
      * @return A correctly formatted string that will create a table with the provided headers and entries.
      */
@@ -65,6 +72,7 @@ public class Transformer
     }
     /**
      * Get a correctly formatted {@link String} that will print out a table.
+     *
      * @param rows The individual rows of the table, including their headers.
      * @return A correctly formatted string that will create a table with the provided headers and entries.
      */
@@ -81,9 +89,10 @@ public class Transformer
     }
     /**
      * Change the behaviour of the table formatting.
+     *
      * @param centeredEntries Weather or not entries should be centered in their column.
-     * @param useSeparators Weather or not separators should be added between columns and rows.
-     * @param entryPadding Weather or not entries should get extra spaces before and after the normal padding.
+     * @param useSeparators   Weather or not separators should be added between columns and rows.
+     * @param entryPadding    Weather or not entries should get extra spaces before and after the normal padding.
      */
     public void setTableSettings(boolean centeredEntries, boolean useSeparators, int entryPadding)
     {
@@ -94,6 +103,7 @@ public class Transformer
 
     /**
      * Get a correctly formatted {@link String} that will print out a graph.
+     *
      * @param values The individual values that get mapped on the xAxis.
      * @return A correctly formatted {@link String}-Array that represents a graph. Can be used to create a table.
      */
@@ -107,12 +117,15 @@ public class Transformer
     }
     /**
      * Change the behaviour of the graph formatting.
-     * @param xAxis How the xAxis should be named.
-     * @param yAxis How the yAxis should be named.
-     * @param xMin What the lowest value on the xAxis is.
-     * @param yMin What the lowestValue on the yAxis is.
-     * @param scaleX How large the steps between individual xValues should be.
-     * @param scaleY How large the steps between individual yValues should be.
+     *
+     * @param xAxis   How the xAxis should be named.
+     * @param yAxis   How the yAxis should be named.
+     * @param xMin    What the lowest value on the xAxis is.
+     * @param yMin    What the lowestValue on the yAxis is.
+     * @param scaleX  How large the steps between individual xValues should be.
+     * @param scaleY  How large the steps between individual yValues should be.
+     * @param xValues How many individual values should be displayed in x-direction.
+     * @param yValues How many individual values should be displayed in y-direction.
      */
     public void setGraphSettings(String xAxis, String yAxis, double xMin, double yMin, double scaleX, double scaleY, int xValues, int yValues)
     {
